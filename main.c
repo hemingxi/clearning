d7fe17b59ffee0101b16207d928f4a7d1f3cde58
hemingxi <hemingxi93@gmail.com>
2023-03-23 15:06:25 +0000

    init commit - hello world

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* main.c */
+int main(int argc, char *argv[]) {
+    printf("Hello World\n");
+
+}
+
+// https://www.freecodecamp.org/news/how-to-write-and-run-c-cpp-code-on-visual-studio-code/
+// need to install code runner extension?
