dfa54cf305bbaf1887d32ac0c6e1f051295b4972
Daniel <hemingxi93@gmail.co>
2023-03-23 21:59:21 -0700

    test commit

--- main.c
+++ main.c
@@ -6,5 +6,8 @@ int main(int argc, char *argv[]) {
 
 }
 
-// https://www.freecodecamp.org/news/how-to-write-and-run-c-cpp-code-on-visual-studio-code/
 // need to install code runner extension?
+// https://www.freecodecamp.org/news/how-to-write-and-run-c-cpp-code-on-visual-studio-code/
+
+// can't push to git:
+// https://stackoverflow.com/questions/68170849/i-get-you-dont-have-permissions-to-push-username-repository-on-github
