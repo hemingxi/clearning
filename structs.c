92a397e7330ad05739d73501710825d880d8a18c
Daniel <hemingxi93@gmail.co>
2023-03-25 08:07:57 -0700

    test2

---
+++ structs.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* link;
+};
+
+struct Node* head;
+
+void Insert(int x) {
+
+}
+
+void Print() {
+
+}
+
+int main(){   
+    head = NULL; 
+}
+
